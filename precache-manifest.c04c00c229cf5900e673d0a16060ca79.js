cbdcb0e13de443c2251ec122bcbdeb13e71669c6
SebastianHoyosLopez <tatansebastan@gmail.com>
2020-09-03 22:48:12 -0500

    Updates

---
+++ precache-manifest.c04c00c229cf5900e673d0a16060ca79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438b5fd750d71b97d6d21a4e55084605",
+    "url": "/Challenge/index.html"
+  },
+  {
+    "revision": "a3a844af041458944376",
+    "url": "/Challenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1a2e4b7631959d689c07",
+    "url": "/Challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3a844af041458944376",
+    "url": "/Challenge/static/js/2.f2f4aa32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Challenge/static/js/2.f2f4aa32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2e4b7631959d689c07",
+    "url": "/Challenge/static/js/main.99b1a1fb.chunk.js"
+  },
+  {
+    "revision": "7eccf52a5a91c73c9bea",
+    "url": "/Challenge/static/js/runtime-main.1505b6bf.js"
+  }
+]);
