dbd77dc0ce24ccc03f35f20940b1610729fe1f75
SebastianHoyosLopez <tatansebastan@gmail.com>
2020-09-03 22:49:13 -0500

    page web update

--- src/componetsChallenge/Challenge-one.jsx
+++ src/componetsChallenge/Challenge-one.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function TextImput() { 
-  const [text, setText] = React.useState('');
-    return (
-      <div>
-        <header className="container">
-          <div className="mt-5 col-6">
-            <strong>Escribe algo en este div: </strong>
-            {text}
-          </div>
-          <input 
-            onChange={(e) => {
-              setText(e.target.value)
-            }} 
-            type="text" 
-            name="textoinput"
-            value={text.textoinput} />
-        </header>
-      </div>  
-    )
-  }
-export default TextImput;
+function TextImput() {
+  const [text, setText] = React.useState('')
+  return (
+    <div>
+      <header className="container">
+        <div className="mt-5 col-6">
+          <strong>Escribe algo en este div: </strong>
+          {text}
+        </div>
+        <input
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
+          type="text"
+          name="textoinput"
+          value={text.textoinput}
+        />
+      </header>
+    </div>
+  )
+}
+export default TextImput
--- src/componetsChallenge/Challenge-two.jsx
+++ src/componetsChallenge/Challenge-two.jsx
@@ -1,90 +1,87 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
+function PersonList() {
+  const [CovidCases, setCovidCases] = useState([])
+  const [textCity, setTextCity] = useState('')
+  const [date, setDate] = useState('')
 
-function PersonList (){
-    const [CovidCases, setCovidCases] = useState([]);
-    const [textCity, setTextCity] = useState('');
-    const [date, setDate] = useState('');
+  const getCovid = () => {
+    axios
+      .get(
+        'https://www.datos.gov.co/resource/gt2j-8ykr.json?ciudad_de_ubicaci_n=' +
+          textCity +
+          '&fecha_diagnostico=' +
+          date +
+          'T00:00:00.000',
+      )
+      .then((res) => {
+        setCovidCases(res.data)
+      })
+  }
 
-    const getCovid =() => {
-        console.log("https://www.datos.gov.co/resource/gt2j-8ykr.json?ciudad_de_ubicaci_n="+ textCity +"&fecha_diagnostico="+ date + "T00:00:00.000")
-        axios
-        .get("https://www.datos.gov.co/resource/gt2j-8ykr.json?ciudad_de_ubicaci_n="+ textCity +"&fecha_diagnostico="+ date + "T00:00:00.000")
-        .then((res)=>{
-            setCovidCases(res.data)
-        })
-    }
-
-    return(
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h4 className="my-5 text-center">Consulta sobre pacientes con Covid-19 en el Oriente Antioqueño </h4>
-                    <div className= "row">
-                        <div className="col">
-                            <input 
-                                className="form-control"
-                                placeholder="Municipio" 
-                                type="text" 
-                                name="cityinput"
-                                value={textCity}
-                                onChange={(e) => {
-                                    setTextCity(e.target.value)
-                                }} />
-                        </div>
-                        <div className="col">
-                            <input 
-                                className ="form-control" 
-                                type ="date"
-                                name="textdate"
-                                value={date}
-                                onChange={(e) => {
-                                    setDate(e.target.value)
-                                }} />
-                        </div>
-                        <div className="col">
-                            <button className="btn btn-success" onClick={getCovid}>Buscar</button>
-                        </div>
-                        <table className="table mt-3">
-                            <thead>
-                                <tr>
-                                <th scope="col">
-                                    Edad
-                                </th>
-                                <th scope="col">
-                                    Sexo
-                                </th>
-                                <th scope="col">
-                                    Tipo
-                                </th>
-                                <th scope="col">
-                                    Estado
-                                </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {CovidCases.map((CovidCase, index ) => {
-                                    return ( 
-                                        <tr key={index}>
-                                            <td>{CovidCase.edad}</td>
-                                            <td>{CovidCase.sexo}</td>
-                                            <td>{CovidCase.tipo}</td>
-                                            <td>{CovidCase.estado}</td>
-                                        </tr>
-                                )})}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h4 className="my-5 text-center">
+            Consulta sobre pacientes con Covid-19 en el Oriente Antioqueño{' '}
+          </h4>
+          <div className="row">
+            <div className="col">
+              <input
+                className="form-control"
+                placeholder="Municipio"
+                type="text"
+                name="cityinput"
+                value={textCity}
+                onChange={(e) => {
+                  setTextCity(e.target.value)
+                }}
+              />
             </div>
-        </div>       
-)}
-export default PersonList                          
-
-
-
-
-
-                                                
-                                               
+            <div className="col">
+              <input
+                className="form-control"
+                type="date"
+                name="textdate"
+                value={date}
+                onChange={(e) => {
+                  setDate(e.target.value)
+                }}
+              />
+            </div>
+            <div className="col">
+              <button className="btn btn-success" onClick={getCovid}>
+                Buscar
+              </button>
+            </div>
+            <table className="table mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Edad</th>
+                  <th scope="col">Sexo</th>
+                  <th scope="col">Tipo</th>
+                  <th scope="col">Estado</th>
+                </tr>
+              </thead>
+              <tbody>
+                {CovidCases.map((CovidCase, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{CovidCase.edad}</td>
+                      <td>{CovidCase.sexo}</td>
+                      <td>{CovidCase.tipo}</td>
+                      <td>{CovidCase.estado}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default PersonList
